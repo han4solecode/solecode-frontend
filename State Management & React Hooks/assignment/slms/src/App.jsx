a294b761b6bde6b3f8cfa984c31ae225e5164a3a
han4solecode <farhan@solecode.id>
2024-10-30 10:50:12 +0700

    create books state to store book data and pass it to BookList component as a prop

--- State Management & React Hooks/assignment/slms/src/App.jsx
+++ State Management & React Hooks/assignment/slms/src/App.jsx
@@ -1,15 +1,31 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Content from "./components/Content";
+import AddBookForm from "./components/AddBookForm";
+import BookList from "./components/BookList";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [books, setBooks] = useState([
+    {
+      id: 1,
+      title: "Book 1",
+      author: "Author 1",
+      category: "Arts & Music",
+      year: 2022,
+      isbn: "anjsk2u492",
+    },
+  ]);
+
   return (
     <>
       <div className="App">
         <Header></Header>
-        <Content></Content>
+        <div className="container d-flex flex-column">
+          <BookList books={books}></BookList>
+          <AddBookForm></AddBookForm>
+        </div>
         <Footer></Footer>
       </div>
     </>
