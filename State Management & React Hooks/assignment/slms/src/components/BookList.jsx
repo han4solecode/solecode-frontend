179add885998ad9e91487f06bcaa92c5dc36b094
han4solecode <farhan@solecode.id>
2024-10-30 10:47:47 +0700

    seperate table body to its own component to handle no table data rendering

--- State Management & React Hooks/assignment/slms/src/components/BookList.jsx
+++ State Management & React Hooks/assignment/slms/src/components/BookList.jsx
@@ -1,36 +1,32 @@
-function BookList() {
-  var i = 1;
+function BookList(props) {
+  const { books } = props;
 
-  const books = [
-    {
-      title: "Book 1",
-      author: "Author 1",
-      category: "Cat 1",
-      year: 2021,
-      isbn: "38797275",
-    },
-    {
-      title: "Book 2",
-      author: "Author 2",
-      category: "Cat 2",
-      year: 2023,
-      isbn: "385297275",
-    },
-    {
-      title: "Book 3",
-      author: "Author 3",
-      category: "Cat 3",
-      year: 2019,
-      isbn: "48790275",
-    },
-    {
-      title: "Book 4",
-      author: "Author 4",
-      category: "Cat 4",
-      year: 2024,
-      isbn: "38797279",
-    },
-  ];
+  const TableBody = () => {
+    return (
+      <tbody>
+        {books.map((book) => (
+          <tr className="align-middle text-center" key={book.id}>
+            <th scope="row" className="text-center">
+              {book.id}
+            </th>
+            <td>{book.title}</td>
+            <td>{book.author}</td>
+            <td>{book.category}</td>
+            <td>{book.year}</td>
+            <td>{book.isbn}</td>
+            <td>
+              <button type="button" className="btn btn-warning mx-1">
+                Update
+              </button>
+              <button type="button" className="btn btn-danger mx-1">
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  };
 
   return (
     <>
@@ -39,7 +35,7 @@ function BookList() {
         <table className="table table-striped w-auto table-bordered table-dark">
           <thead>
             <tr className="text-center">
-              <th scope="col">No</th>
+              <th scope="col">ID</th>
               <th scope="col">Title</th>
               <th scope="col">Author</th>
               <th scope="col">Book Category</th>
@@ -48,28 +44,15 @@ function BookList() {
               <th scope="col">Action</th>
             </tr>
           </thead>
-          <tbody>
-            {books.map((book, key) => (
-              <tr className="align-middle">
-                <th scope="row" className="text-center">
-                  {i++}
-                </th>
-                <td>{book.title}</td>
-                <td>{book.author}</td>
-                <td>{book.category}</td>
-                <td>{book.year}</td>
-                <td>{book.isbn}</td>
-                <td>
-                  <button type="button" className="btn btn-warning mx-1">
-                    Update
-                  </button>
-                  <button type="button" className="btn btn-danger mx-1">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          {books.length ? (
+            <TableBody></TableBody>
+          ) : (
+            <tr className="bg-white">
+              <td colSpan="7" className="text-black text-center">
+                No Data Available
+              </td>
+            </tr>
+          )}
         </table>
       </div>
     </>
