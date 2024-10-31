87ab24348097fb28c397f67bf4bfc0e79b003379
han4solecode <farhan@solecode.id>
2024-10-31 12:08:23 +0700

    adding add new menu functionality

---
+++ State Management & React Hooks/mini-project/ofos/src/components/Menu/AddMenuForm.jsx
@@ -0,0 +1,170 @@
+import { useState } from "react";
+
+function AddMenuForm(props) {
+  const { menus, onAddMenu } = props;
+
+  const categories = ["Food", "Beverage", "Dessert"];
+
+  const initialValues = {
+    id: 0,
+    name: "",
+    price: 0,
+    category: categories[0],
+    rating: 0,
+    isAvailable: true,
+  };
+
+  const [formValues, setFormValues] = useState(initialValues);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    console.log(formValues);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (menus.length === 0) {
+      var id = 1;
+    } else {
+      var id = menus[menus.length - 1].id + 1;
+    }
+
+    let newMenu = {
+      id: id,
+      name: formValues.name,
+      price: formValues.price,
+      category: formValues.category,
+      rating: formValues.rating,
+      isAvailable: formValues.isAvailable,
+    };
+
+    onAddMenu(newMenu);
+
+    setFormValues(initialValues);
+
+    alert(`A menu with ID ${newMenu.id} has been created successfully`);
+  };
+
+  return (
+    <>
+      <form id="addMenuForm" autoComplete="off" onSubmit={handleFormSubmit}>
+        <caption className="row">
+          <span>Add a New Menu</span>
+        </caption>
+        <div className="my-1 mb-4 row">
+          <label htmlFor="inputMenuName" className="col-sm-2 col-form-label">
+            Name
+          </label>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              id="inputMenuName"
+              name="name"
+              required
+              minLength="2"
+              maxLength="100"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="my-3 mb-4 row">
+          <label htmlFor="inputMenuPrice" className="col-sm-2 col-form-label">
+            Price
+          </label>
+          <div className="col">
+            <input
+              type="number"
+              className="form-control"
+              id="inputMenuPrice"
+              name="price"
+              required
+              min="0"
+              max="100000"
+              value={formValues.price}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="my-3 mb-4 row">
+          <label
+            htmlFor="inputMenuCategory"
+            className="col-sm-2 col-form-label"
+          >
+            Category
+          </label>
+          <div className="col">
+            <select
+              name="category"
+              id="inputMenuCategory"
+              className="form-select"
+              value={formValues.category}
+              onChange={handleInputChange}
+            >
+              {categories.map((val, key) => (
+                <option value={val} key={key}>
+                  {val}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="my-3 mb-4 row">
+          <label htmlFor="inputMenuRating" className="col-sm-2 col-form-label">
+            Rating
+          </label>
+          <div className="col">
+            <input
+              type="number"
+              className="form-control"
+              id="inputMenuRating"
+              name="rating"
+              required
+              min="0"
+              max="5"
+              value={formValues.rating}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="my-3 mb-4 row">
+          <label
+            htmlFor="inputMenuAvailability"
+            className="col-sm-2 col-form-label"
+          >
+            Availability
+          </label>
+          <div className="col">
+            <select
+              name="isAvailable"
+              id="inputMenuAvailability"
+              className="form-select"
+              value={formValues.isAvailable}
+              onChange={handleInputChange}
+            >
+              <option value={true}>Yes</option>
+              <option value={false}>No</option>
+            </select>
+          </div>
+        </div>
+        <button
+          type="submit"
+          style={{
+            backgroundColor: "#A28B55",
+            color: "white",
+            border: "none",
+            padding: "10px 15px",
+            borderRadius: "4px",
+          }}
+        >
+          Add
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default AddMenuForm;
--- State Management & React Hooks/mini-project/ofos/src/pages/Menu.jsx
+++ State Management & React Hooks/mini-project/ofos/src/pages/Menu.jsx
@@ -13,7 +13,7 @@ function Menu(props) {
       price: 20000,
       category: "Food",
       rating: 5,
-      isAvailable: true,
+      isAvailable: "true",
     },
     {
       id: 2,
@@ -21,7 +21,7 @@ function Menu(props) {
       price: 40000,
       category: "Food",
       rating: 4,
-      isAvailable: true,
+      isAvailable: "true",
     },
     {
       id: 3,
@@ -29,7 +29,7 @@ function Menu(props) {
       price: 7000,
       category: "Beverage",
       rating: 4,
-      isAvailable: true,
+      isAvailable: "true",
     },
     {
       id: 4,
@@ -37,7 +37,7 @@ function Menu(props) {
       price: 14000,
       category: "Beverage",
       rating: 4,
-      isAvailable: false,
+      isAvailable: "false",
     },
     {
       id: 5,
@@ -45,22 +45,10 @@ function Menu(props) {
       price: 10000,
       category: "Dessert",
       rating: 3,
-      isAvailable: true,
+      isAvailable: "true",
     },
   ];
 
-  const categories = ["Food", "Beverage", "Dessert"];
-
-  const initialValues = {
-    id: 0,
-    name: "",
-    price: 0,
-    category: categories[0],
-    rating: 0,
-    isAvailable: true,
-  };
-
-  const [formValues, setFormValues] = useState(initialValues);
   const [menus, setMenus] = useState(initialMenu);
 
   const [menuToEdit, setMenuToEdit] = useState(null);
@@ -80,222 +68,12 @@ function Menu(props) {
   const handleAddMenu = (newMenu) => {
     setMenus([...menus, newMenu]);
   };
+  // end porps to AddMenuForm
 
   useEffect(() => {
     sendDataToApp(menus);
-  }, []);
+  }, [menus]);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-    console.log(formValues);
-  };
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    setMenus([...menus, formValues]);
-  };
-
-  // return (
-  //   <>
-  //     <div className="container d-flex my-3 flex-column">
-  //       <div className="row">
-  //         <div className="col border-top">
-  //           <h2 style={{ color: "#86AB89" }}>Menus</h2>
-  //         </div>
-  //       </div>
-  //       <div className="row">
-  //         <div className="col">
-  //           <table className="table caption-top w-auto align-middle table-striped">
-  //             <caption>List of Menus</caption>
-  //             <thead>
-  //               <tr className="text-center">
-  //                 <th scope="col">ID</th>
-  //                 <th scope="col">Name</th>
-  //                 <th scope="col">Price</th>
-  //                 <th scope="col">Category</th>
-  //                 <th scope="col">Rating</th>
-  //                 <th scope="col">Availability</th>
-  //                 <th scope="col">Action</th>
-  //               </tr>
-  //             </thead>
-  //             <tbody>
-  //               {menus.map((menu) => (
-  //                 <tr className="align-middle">
-  //                   <td className="text-center">{menu.id}</td>
-  //                   <td className="text-center">{menu.name}</td>
-  //                   <td className="text-center">{menu.price}</td>
-  //                   <td className="text-center">{menu.category}</td>
-  //                   <td className="text-center">{menu.rating}</td>
-  //                   {menu.isAvailable ? (
-  //                     <td className="text-center">Yes</td>
-  //                   ) : (
-  //                     <td className="text-center">No</td>
-  //                   )}
-  //                   <td className="text-center">
-  //                     <a href="" className="text-decoration-none">
-  //                       Update
-  //                     </a>{" "}
-  //                     |{" "}
-  //                     <a href="" className="text-decoration-none">
-  //                       Delete
-  //                     </a>
-  //                   </td>
-  //                 </tr>
-  //               ))}
-  //             </tbody>
-  //           </table>
-  //         </div>
-  //         <div className="col">
-  //           <form
-  //             id="addMenuForm"
-  //             autoComplete="off"
-  //             onSubmit={handleFormSubmit}
-  //           >
-  //             <caption className="row">
-  //               <span>Add a New Menu</span>
-  //             </caption>
-  //             <div className="my-1 mb-4 row">
-  //               <label
-  //                 htmlFor="inputMenuId"
-  //                 className="col-sm-2 col-form-label"
-  //               >
-  //                 ID
-  //               </label>
-  //               <div className="col">
-  //                 <input
-  //                   type="number"
-  //                   className="form-control"
-  //                   id="inputMenuId"
-  //                   name="id"
-  //                   required
-  //                   value={formValues.id}
-  //                   onChange={handleInputChange}
-  //                 />
-  //               </div>
-  //             </div>
-  //             <div className="my-3 mb-4 row">
-  //               <label
-  //                 htmlFor="inputMenuName"
-  //                 className="col-sm-2 col-form-label"
-  //               >
-  //                 Name
-  //               </label>
-  //               <div className="col">
-  //                 <input
-  //                   type="text"
-  //                   className="form-control"
-  //                   id="inputMenuName"
-  //                   name="name"
-  //                   required
-  //                   minLength="2"
-  //                   maxLength="100"
-  //                   value={formValues.name}
-  //                   onChange={handleInputChange}
-  //                 />
-  //               </div>
-  //             </div>
-  //             <div className="my-3 mb-4 row">
-  //               <label
-  //                 htmlFor="inputMenuPrice"
-  //                 className="col-sm-2 col-form-label"
-  //               >
-  //                 Price
-  //               </label>
-  //               <div className="col">
-  //                 <input
-  //                   type="number"
-  //                   className="form-control"
-  //                   id="inputMenuPrice"
-  //                   name="price"
-  //                   required
-  //                   min="0"
-  //                   max="100000"
-  //                   value={formValues.price}
-  //                   onChange={handleInputChange}
-  //                 />
-  //               </div>
-  //             </div>
-  //             <div className="my-3 mb-4 row">
-  //               <label
-  //                 htmlFor="inputMenuCategory"
-  //                 className="col-sm-2 col-form-label"
-  //               >
-  //                 Category
-  //               </label>
-  //               <div className="col">
-  //                 <select
-  //                   name="category"
-  //                   id="inputMenuCategory"
-  //                   className="form-select"
-  //                   value={formValues.category}
-  //                   onChange={handleInputChange}
-  //                 >
-  //                   {categories.map((val) => (
-  //                     <option value={val}>{val}</option>
-  //                   ))}
-  //                 </select>
-  //               </div>
-  //             </div>
-  //             <div className="my-3 mb-4 row">
-  //               <label
-  //                 htmlFor="inputMenuRating"
-  //                 className="col-sm-2 col-form-label"
-  //               >
-  //                 Rating
-  //               </label>
-  //               <div className="col">
-  //                 <input
-  //                   type="number"
-  //                   className="form-control"
-  //                   id="inputMenuRating"
-  //                   name="rating"
-  //                   required
-  //                   min="0"
-  //                   max="5"
-  //                   value={formValues.rating}
-  //                   onChange={handleInputChange}
-  //                 />
-  //               </div>
-  //             </div>
-  //             <div className="my-3 mb-4 row">
-  //               <label
-  //                 htmlFor="inputMenuAvailability"
-  //                 className="col-sm-2 col-form-label"
-  //               >
-  //                 Availability
-  //               </label>
-  //               <div className="col">
-  //                 <select
-  //                   name="isAvailable"
-  //                   id="inputMenuAvailability"
-  //                   className="form-select"
-  //                   value={formValues.isAvailable}
-  //                   onChange={handleInputChange}
-  //                 >
-  //                   <option value={true}>Yes</option>
-  //                   <option value={false}>No</option>
-  //                 </select>
-  //               </div>
-  //             </div>
-  //             <button
-  //               type="submit"
-  //               style={{
-  //                 backgroundColor: "#A28B55",
-  //                 color: "white",
-  //                 border: "none",
-  //                 padding: "10px 15px",
-  //                 borderRadius: "4px",
-  //               }}
-  //             >
-  //               Submit
-  //             </button>
-  //           </form>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </>
-  // );
   return (
     <>
       <div className="container d-flex my-3 flex-column">
@@ -304,24 +82,26 @@ function Menu(props) {
             <h2 style={{ color: "#86AB89" }}>Menus</h2>
           </div>
         </div>
-        <div className="row">
+        <div className="container">
           <div className="col">
-            <MenuList
-              menus={menus}
-              onEdit={handleMenuEditMode}
-              onDelete={handleDeleteMenu}
-            ></MenuList>
-          </div>
-          <div className="col">
-            <AddMenuForm
-              menus={menus}
-              onAddMenu={handleAddMenu}
-              // onUpdateMenu={handleUpdateMenu}
-              // editingMenu={
-              //   menuToEdit !== null && menus.find((menu) => menu.id === menuToEdit)
-              // }
-              // onDoneUpdate={handleDoneUpdateMenu}
-            ></AddMenuForm>
+            <div className="row">
+              <MenuList
+                menus={menus}
+                onEdit={handleMenuEditMode}
+                onDelete={handleDeleteMenu}
+              ></MenuList>
+            </div>
+            <div className="row">
+              <AddMenuForm
+                menus={menus}
+                onAddMenu={handleAddMenu}
+                // onUpdateMenu={handleUpdateMenu}
+                // editingMenu={
+                //   menuToEdit !== null && menus.find((menu) => menu.id === menuToEdit)
+                // }
+                // onDoneUpdate={handleDoneUpdateMenu}
+              ></AddMenuForm>
+            </div>
           </div>
         </div>
       </div>
