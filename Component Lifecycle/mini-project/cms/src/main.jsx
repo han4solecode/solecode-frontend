788c9a6163efc8b5b871b42ceb12cd2af1469a55
han4solecode <farhan@solecode.id>
2024-11-07 09:32:05 +0700

    setup react router dom

--- Component Lifecycle/mini-project/cms/src/main.jsx
+++ Component Lifecycle/mini-project/cms/src/main.jsx
@@ -1,8 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
-// import App from './App.jsx'
+// import App from "./App.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello</div>,
+    errorElement: (
+      <div className="flex justify-center items-center h-screen">Error 404</div>
+    ),
+    children: [],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>{/* <App /> */}</StrictMode>
+  <StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
 );
