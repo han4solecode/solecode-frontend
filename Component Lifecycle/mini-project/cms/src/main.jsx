d56c3781e7d2d1a8c10d9c7c79eedcdf8da9808e
han4solecode <farhan@solecode.id>
2024-11-07 23:31:16 +0700

    handle all page route

--- Component Lifecycle/mini-project/cms/src/main.jsx
+++ Component Lifecycle/mini-project/cms/src/main.jsx
@@ -6,15 +6,99 @@ import "./index.css";
 // import App from "./App.jsx";
 
 import MainLayout from "./components/Layouts/MainLayout";
+import DashboardPage from "./pages/DashboardPage";
+import DepartmentsPage from "./pages/DepartmentsPage";
+import DepartmentFormPage from "./pages/DepartmentFormPage";
+import EmployeesPage from "./pages/EmployeesPage";
+import EmployeeFormPage from "./pages/EmployeeFormPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ProjectFormPage from "./pages/ProjectFormPage";
+import AssignmentsPage from "./pages/AssignmentsPage";
+import AssignmentFormPage from "./pages/AssignmentFormPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
     errorElement: (
-      <div className="flex justify-center items-center h-screen">Error 404</div>
+      <div className="flex justify-center items-center h-screen text-3xl">
+        Error 404
+      </div>
     ),
-    children: [],
+    children: [
+      {
+        path: "",
+        element: <DashboardPage></DashboardPage>,
+      },
+      {
+        path: "/employees",
+        element: <EmployeesPage></EmployeesPage>,
+      },
+      {
+        path: "/employees/new",
+        element: <EmployeeFormPage></EmployeeFormPage>,
+      },
+      {
+        path: "/employees/:id",
+        element: <EmployeeFormPage isEditing={true}></EmployeeFormPage>,
+        errorElement: (
+          <div className="flex justify-center items-center h-screen text-3xl">
+            Error 404
+          </div>
+        ),
+      },
+      {
+        path: "/departments",
+        element: <DepartmentsPage></DepartmentsPage>,
+      },
+      {
+        path: "/departments/new",
+        element: <DepartmentFormPage></DepartmentFormPage>,
+      },
+      {
+        path: "/departments/:id",
+        element: <DepartmentFormPage isEditing={true}></DepartmentFormPage>,
+        errorElement: (
+          <div className="flex justify-center items-center h-screen text-3xl">
+            Error 404
+          </div>
+        ),
+      },
+      {
+        path: "/projects",
+        element: <ProjectsPage></ProjectsPage>,
+      },
+      {
+        path: "/projects/new",
+        element: <ProjectFormPage></ProjectFormPage>,
+      },
+      {
+        path: "/projects/:id",
+        element: <ProjectFormPage isEditing={true}></ProjectFormPage>,
+        errorElement: (
+          <div className="flex justify-center items-center h-screen text-3xl">
+            Error 404
+          </div>
+        ),
+      },
+      {
+        path: "/assignments",
+        element: <AssignmentsPage></AssignmentsPage>,
+      },
+      {
+        path: "/assignments/new",
+        element: <AssignmentFormPage></AssignmentFormPage>,
+      },
+      {
+        path: "/assignments/:empNo/:projNo",
+        element: <AssignmentFormPage isEditing={true}></AssignmentFormPage>,
+        errorElement: (
+          <div className="flex justify-center items-center h-screen text-3xl">
+            Error 404
+          </div>
+        ),
+      },
+    ],
   },
 ]);
 
