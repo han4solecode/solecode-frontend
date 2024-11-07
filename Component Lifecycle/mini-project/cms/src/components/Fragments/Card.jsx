eb4851bcff447059ff9d6b41d1b0092e9215c114
han4solecode <farhan@solecode.id>
2024-11-07 10:08:36 +0700

    create Card fragment

---
+++ Component Lifecycle/mini-project/cms/src/components/Fragments/Card.jsx
@@ -0,0 +1,25 @@
+import LoadingAnimation from "../Elements/LoadingAnimation";
+
+function Card(props) {
+  const { cardTitle, data, cardFooter } = props;
+
+  return (
+    <div className="w-64 h-40 rounded shadow-lg">
+      <header className="bg-gray-800 text-white text-lg h-10 rounded-t-lg flex justify-center items-center">
+        {cardTitle}
+      </header>
+      <div className="flex flex-col items-center justify-center mt-3">
+        {data ? (
+          <span className="text-6xl">{data}</span>
+        ) : data === 0 ? (
+          <span className="text-6xl">{data}</span>
+        ) : (
+          <LoadingAnimation></LoadingAnimation>
+        )}
+        <span className="mt-1">{cardFooter}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
