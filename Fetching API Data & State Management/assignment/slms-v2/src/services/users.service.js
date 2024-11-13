d5dd18d322ef65f8c7c760b3a1194e1bd1e746ad
han4solecode <farhan@solecode.id>
2024-11-13 12:34:58 +0700

    create get user by id logic

--- Fetching API Data & State Management/assignment/slms-v2/src/services/users.service.js
+++ Fetching API Data & State Management/assignment/slms-v2/src/services/users.service.js
@@ -10,3 +10,14 @@ export const getAllUsers = async () => {
     console.log(`Error: ${error}`);
   }
 };
+
+export const getUserById = async (id) => {
+  try {
+    const res = await axios.get(`/api/user/${id}`);
+    console.log(res.data);
+    const data = res.data;
+    return data;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
