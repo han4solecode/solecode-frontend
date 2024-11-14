ef3fc78fe8ed87a8e3db124bbb04bb33152a40d3
han4solecode <farhan@solecode.id>
2024-11-14 21:36:29 +0700

    create get all employees with no pagination api call function

--- Fetching API Data & State Management/mini-project/cms/src/services/employees.service.js
+++ Fetching API Data & State Management/mini-project/cms/src/services/employees.service.js
@@ -12,6 +12,16 @@ export const getAllEmployees = async (recordsPerPage, currentPage) => {
   }
 };
 
+export const getAllEmployeesNoPaging = async () => {
+  try {
+    const res = await axios.get(`/api/employee/all`);
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
 export const getEmployeeById = async (employeeId) => {
   try {
     const res = await axios.get(`/api/employee/${employeeId}`);
