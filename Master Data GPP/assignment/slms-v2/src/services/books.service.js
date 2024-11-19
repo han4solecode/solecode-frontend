8be4da3a2ff25118fdbb673c64afdace0ff50204
han4solecode <farhan@solecode.id>
2024-11-19 11:56:38 +0700

    fix search book axios get logic (use query param instead of full url)

--- Master Data GPP/assignment/slms-v2/src/services/books.service.js
+++ Master Data GPP/assignment/slms-v2/src/services/books.service.js
@@ -11,23 +11,9 @@ export const getAllBooks = async () => {
   }
 };
 
-export const searchBooks = async (
-  pageNumber,
-  PageSize,
-  keyword,
-  sortBy,
-  sortOrder,
-  title,
-  author,
-  isbn
-) => {
+export const searchBooks = async (params) => {
   try {
-    const res = await axios.get(
-      `/api/book/s?pageNumber=${pageNumber}&pageSize=${PageSize}&keyword=${keyword}&sortBy=${sortBy}&sortOrder=${sortOrder}&title=${title}&author=${author}&isbn=${isbn}`
-    );
-    console.log(res.data);
-    const data = res.data;
-    return data;
+    return await axios.get(`/api/book/s`, { params });
   } catch (error) {
     console.log(`Error: ${error}`);
   }
