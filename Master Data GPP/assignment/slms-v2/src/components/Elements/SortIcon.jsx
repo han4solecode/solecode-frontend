19066fe22f154d060c3c47537d227e4a9f5dff38
han4solecode <farhan@solecode.id>
2024-11-19 15:22:42 +0700

    update sort icon element style

--- Master Data GPP/assignment/slms-v2/src/components/Elements/SortIcon.jsx
+++ Master Data GPP/assignment/slms-v2/src/components/Elements/SortIcon.jsx
@@ -1,21 +1,15 @@
 import { FaArrowsUpDown, FaArrowUp, FaArrowDown } from "react-icons/fa6";
 
-FaUpDown;
-
 function SortIcon(props) {
   const { sortField, field, sortOrder } = props;
 
   if (sortField !== field) {
-    return (
-      <FaArrowsUpDown
-        style={{ color: "white", fontSize: "1.5em" }}
-      ></FaArrowsUpDown>
-    );
+    return <FaArrowsUpDown style={{ color: "white" }}></FaArrowsUpDown>;
   }
   return sortOrder === "asc" ? (
-    <FaArrowUp style={{ color: "white", fontSize: "1.5em" }}></FaArrowUp>
+    <FaArrowUp style={{ color: "white" }}></FaArrowUp>
   ) : (
-    <FaArrowDown style={{ color: "white", fontSize: "1.5em" }}></FaArrowDown>
+    <FaArrowDown style={{ color: "white" }}></FaArrowDown>
   );
 }
 
