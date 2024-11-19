8e74742ba685a4254417e1b711d5dbfdd6f6cbf9
han4solecode <farhan@solecode.id>
2024-11-19 10:36:46 +0700

    create search books api call

--- Master Data GPP/assignment/slms-v2/src/services/books.service.js
+++ Master Data GPP/assignment/slms-v2/src/services/books.service.js
@@ -11,6 +11,28 @@ export const getAllBooks = async () => {
   }
 };
 
+export const searchBooks = async (
+  pageNumber,
+  PageSize,
+  keyword,
+  sortBy,
+  sortOrder,
+  title,
+  author,
+  isbn
+) => {
+  try {
+    const res = await axios.get(
+      `/api/book/s?pageNumber=${pageNumber}&pageSize=${PageSize}&keyword=${keyword}&sortBy=${sortBy}&sortOrder=${sortOrder}&title=${title}&author=${author}&isbn=${isbn}`
+    );
+    console.log(res.data);
+    const data = res.data;
+    return data;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
 export const getBookById = async (id) => {
   try {
     const res = await axios.get(`/api/book/${id}`);
