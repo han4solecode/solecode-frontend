d3c3116889501daf2357d03df83e725049e51aae
han4solecode <farhan@solecode.id>
2024-11-21 11:04:34 +0700

    add QueryClientProvider component from tanstack query to wrap RouterProvider components

--- Master Data GPP/mini-project/cms/src/main.jsx
+++ Master Data GPP/mini-project/cms/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 // import App from "./App.jsx";
@@ -142,9 +143,13 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <RouterProvider router={router}></RouterProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
