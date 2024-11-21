49b0ddb0b7d70b18de31aa6ee4730749fe73b02a
han4solecode <farhan@solecode.id>
2024-11-21 20:03:37 +0700

    implement create new employee functionality

--- Master Data GPP/mini-project/cms/src/pages/EmployeeFormPage.jsx
+++ Master Data GPP/mini-project/cms/src/pages/EmployeeFormPage.jsx
@@ -9,7 +9,10 @@ import {
   getEmployeeById,
   updateEmployee,
 } from "../services/employees.service";
-import { getAllDepartmentNoPaging } from "../services/departments.service";
+import {
+  getAllDepartmentNoPaging,
+  getDepartmentById,
+} from "../services/departments.service";
 
 function EmployeeFormPage(props) {
   const { isEditing } = props;
@@ -18,7 +21,6 @@ function EmployeeFormPage(props) {
   const navigate = useNavigate();
 
   const initialValues = {
-    // empNo: 0,
     fname: "",
     lname: "",
     address: "",
@@ -26,12 +28,20 @@ function EmployeeFormPage(props) {
     sex: "",
     position: "",
     deptno: "",
+    ssn: "",
+    phonenumber: "",
+    email: "",
+    employmenttype: "",
+    salary: "",
+    supervisorempno: "",
+    level: "",
   };
 
   const [formValues, setFormValues] = useState(initialValues);
   const [departments, setDepartments] = useState([]);
   const [errors, setErrors] = useState(initialValues);
   const [loading, setLoading] = useState(false);
+  const [availableSupervisors, setAvailableSupervisors] = useState([]);
 
   useEffect(() => {
     setLoading(true);
@@ -73,15 +83,33 @@ function EmployeeFormPage(props) {
     }
   }, []);
 
+  useEffect(() => {
+    getDepartmentById(Number(formValues.deptno))
+      .then((res) => {
+        // console.log(res.data);
+        setAvailableSupervisors(res.data.employees);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(
+          `Error occurred. Please try again or contact admin. ERROR ${err}`
+        );
+      });
+  }, [formValues.deptno]);
+
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
     if (type === "checkbox") {
       setFormValues({ ...formValues, isAvailable: checked });
     } else {
       setFormValues({ ...formValues, [name]: value });
+      if (name === "deptno") {
+        setFormValues({ ...formValues, supervisorempno: "", deptno: value });
+      }
     }
-    console.log(formValues);
   };
+
+  console.log(formValues);
 
   const handleClearForm = (e) => {
     e.preventDefault();
@@ -94,6 +122,7 @@ function EmployeeFormPage(props) {
     // validation
     let currentDate = new Date().toISOString().slice(0, 10);
     console.log(currentDate);
+    let phoneNumberRegex = /^(0)8[1-9][0-9]{6,9}$/;
     let errorMessages = {};
 
     if (!formValues.fname.trim()) {
@@ -104,7 +133,9 @@ function EmployeeFormPage(props) {
       errorMessages.fname = "";
     }
 
-    if (formValues.lname.length > 50) {
+    if (!formValues.lname.trim()) {
+      errorMessages.lname = "Last name is required";
+    } else if (formValues.lname.length > 50) {
       errorMessages.lname = "Last name cannot exceed 50 characters";
     } else {
       errorMessages.lname = "";
@@ -144,6 +175,46 @@ function EmployeeFormPage(props) {
       errorMessages.deptno = "";
     }
 
+    if (!formValues.email) {
+      errorMessages.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formValues.email)) {
+      errorMessages.email = "Email is not valid";
+    } else {
+      errorMessages.email = "";
+    }
+
+    if (!formValues.phonenumber) {
+      errorMessages.phonenumber = "Phone number is required";
+    } else if (!phoneNumberRegex.test(formValues.phonenumber)) {
+      errorMessages.phonenumber = "Phone number is not valid";
+    } else {
+      errorMessages.phonenumber = "";
+    }
+
+    if (!formValues.ssn) {
+      errorMessages.ssn = "SSN is required";
+    } else {
+      errorMessages.ssn = "";
+    }
+
+    if (!formValues.employmenttype) {
+      errorMessages.employmenttype = "Employment type is required";
+    } else {
+      errorMessages.employmenttype = "";
+    }
+
+    if (!formValues.salary) {
+      errorMessages.salary = "Salary is required";
+    } else {
+      errorMessages.salary = "";
+    }
+
+    if (!formValues.level) {
+      errorMessages.level = "Level is required";
+    } else {
+      errorMessages.level = "";
+    }
+
     setErrors(errorMessages);
 
     let formValid = true;
@@ -174,7 +245,17 @@ function EmployeeFormPage(props) {
             );
           });
       } else {
-        let newEmployee = { ...formValues, deptno: Number(formValues.deptno) };
+        let newEmployee = {
+          ...formValues,
+          deptno: Number(formValues.deptno),
+          salary: Number(formValues.salary),
+          supervisorempno:
+            formValues.supervisorempno === null
+              ? formValues.supervisorempno
+              : Number(formValues.supervisorempno),
+          level: Number(formValues.level),
+          status: "Active",
+        };
         addEmployee(newEmployee)
           .then((res) => {
             if (res.status === 201) {
@@ -206,24 +287,57 @@ function EmployeeFormPage(props) {
     >
       <div className="w-full">
         <form autoComplete="off">
-          <FormInput
-            name="fname"
-            type="text"
-            onChange={(e) => handleInputChange(e)}
-            value={formValues.fname}
-            errorMessage={errors.fname}
-          >
-            First Name
-          </FormInput>
-          <FormInput
-            name="lname"
-            type="text"
-            onChange={(e) => handleInputChange(e)}
-            value={formValues.lname}
-            errorMessage={errors.lname}
-          >
-            Last Name
-          </FormInput>
+          <div className="flex gap-4">
+            <FormInput
+              name="fname"
+              type="text"
+              onChange={(e) => handleInputChange(e)}
+              value={formValues.fname}
+              errorMessage={errors.fname}
+            >
+              First Name
+            </FormInput>
+            <FormInput
+              name="lname"
+              type="text"
+              onChange={(e) => handleInputChange(e)}
+              value={formValues.lname}
+              errorMessage={errors.lname}
+            >
+              Last Name
+            </FormInput>
+          </div>
+          <div className="flex gap-4">
+            <FormInput
+              name="email"
+              type="text"
+              onChange={(e) => handleInputChange(e)}
+              value={formValues.email}
+              errorMessage={errors.email}
+              placeholder="example@email.com"
+            >
+              Email
+            </FormInput>
+            <FormInput
+              name="phonenumber"
+              type="text"
+              onChange={(e) => handleInputChange(e)}
+              value={formValues.phonenumber}
+              errorMessage={errors.phonenumber}
+              placeholder="08XXXXXXXX"
+            >
+              Phone Number
+            </FormInput>
+            <FormInput
+              name="ssn"
+              type="text"
+              onChange={(e) => handleInputChange(e)}
+              value={formValues.ssn}
+              errorMessage={errors.ssn}
+            >
+              Social Security Number
+            </FormInput>
+          </div>
           <FormInput
             name="address"
             type="text"
@@ -286,15 +400,6 @@ function EmployeeFormPage(props) {
             </div>
             {errors.sex && <small className="text-red-600">{errors.sex}</small>}
           </div>
-          <FormInput
-            name="position"
-            type="text"
-            onChange={(e) => handleInputChange(e)}
-            value={formValues.position}
-            errorMessage={errors.position}
-          >
-            Position
-          </FormInput>
           <div className="mb-3">
             <label
               htmlFor="deptno"
@@ -320,6 +425,89 @@ function EmployeeFormPage(props) {
             </select>
             {errors.deptno && (
               <small className="text-red-600">{errors.deptno}</small>
+            )}
+          </div>
+          <div className="flex items-center gap-4">
+            <FormInput
+              name="position"
+              type="text"
+              onChange={(e) => handleInputChange(e)}
+              value={formValues.position}
+              errorMessage={errors.position}
+            >
+              Position
+            </FormInput>
+            <div className="mb-3 w-full">
+              <label
+                htmlFor="employmenttype"
+                className="block text-black mb-2 text-lg text-semibold"
+              >
+                Employment Type
+              </label>
+              <select
+                name="employmenttype"
+                id="employmenttype"
+                className="border rounded text-lg w-full p-2 border-gray-400 focus:border-gray-800 focus:ring-0 focus:outline-none"
+                value={formValues.employmenttype}
+                onChange={(e) => handleInputChange(e)}
+              >
+                <option value="" disabled hidden>
+                  Select Employment Type
+                </option>
+                <option value="Permanent">Permanent</option>
+                <option value="Contract">Contract</option>
+              </select>
+              {errors.employmenttype && (
+                <small className="text-red-600">{errors.employmenttype}</small>
+              )}
+            </div>
+            <FormInput
+              name="salary"
+              type="number"
+              onChange={(e) => handleInputChange(e)}
+              value={formValues.salary}
+              errorMessage={errors.salary}
+              min="0"
+            >
+              Salary
+            </FormInput>
+            <FormInput
+              name="level"
+              type="number"
+              onChange={(e) => handleInputChange(e)}
+              value={formValues.level}
+              errorMessage={errors.level}
+              min="0"
+            >
+              Level
+            </FormInput>
+          </div>
+          <div className="mb-3 w-full">
+            <label
+              htmlFor="supervisorempno"
+              className="block text-black mb-2 text-lg text-semibold"
+            >
+              Supervisor
+            </label>
+            {availableSupervisors !== undefined ? (
+              <select
+                name="supervisorempno"
+                id="supervisorempno"
+                className="border rounded text-lg w-full p-2 border-gray-400 focus:border-gray-800 focus:ring-0 focus:outline-none"
+                value={formValues.supervisorempno}
+                onChange={(e) => handleInputChange(e)}
+              >
+                <option value={null} hidden>
+                  Select Supervisor
+                </option>
+                {availableSupervisors.map((sup) => (
+                  <option value={sup.empno} key={sup.empno}>
+                    {sup.fname} {sup.lname}
+                  </option>
+                ))}
+              </select>
+            ) : (
+              <span className="text-red-600">No Supervisor Available</span>
             )}
           </div>
           <div className="mt-3 space-x-2">
