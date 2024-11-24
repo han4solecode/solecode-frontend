1271b8f048eb04a06cad97fdf44f09eb0a484785
han4solecode <farhan@solecode.id>
2024-11-24 23:42:45 +0700

    create api call service for authentication

---
+++ Authentication & Authorization GPP/assignment/slms-v2/src/services/auth.service.js
@@ -0,0 +1,39 @@
+import api from "../Api";
+
+const register = async (userData) => {
+  try {
+    const res = await api.post("/auth/register", userData);
+    return res.data;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
+const login = async (userData) => {
+  try {
+    const res = await api.post("/auth/login", userData);
+    if (res.data.token) {
+      localStorage.setItem("user", JSON.stringify(res.data));
+    }
+    return res.data;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
+const logout = async () => {
+  try {
+    await api.post("/auth/logout");
+    localStorage.removeItem("user");
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
+const AuthService = {
+  register,
+  login,
+  logout,
+};
+
+export default AuthService;
