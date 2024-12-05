f16110febe296b120286fe1932b76e91b752abd0
han4solecode <farhan@solecode.id>
2024-12-06 02:33:51 +0700

    change axios instance

--- Workflow GPP/mini-project/cms/src/services/auth.service.js
+++ Workflow GPP/mini-project/cms/src/services/auth.service.js
@@ -1,24 +1,25 @@
 import api from "../Api";
+import AxiosInstance from "../Api";
 
 const register = async (userData) => {
-  const res = await api.post("/api/employee/create", userData);
+  const res = await AxiosInstance.api.post("/api/employee/create", userData);
   return res;
 };
 
 const login = async (userData) => {
-  const res = await api.post("/api/auth/login", userData);
+  const res = await AxiosInstance.api.post("/api/auth/login", userData);
   localStorage.setItem("user", JSON.stringify(res.data));
   return res;
 };
 
 const logout = async () => {
-  const res = await api.post("/api/auth/logout");
+  const res = await AxiosInstance.api.post("/api/auth/logout");
   localStorage.removeItem("user");
   return res;
 };
 
 const refreshToken = async () => {
-  const res = await api.post("/api/auth/refresh-token");
+  const res = await AxiosInstance.api.post("/api/auth/refresh-token");
   localStorage.setItem("user", JSON.stringify(res.data));
   return res;
 };
