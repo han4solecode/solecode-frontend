af44f9d363040eb8cb41d1c51372e195a25b3415
han4solecode <farhan@solecode.id>
2024-12-06 02:32:38 +0700

    add another axios instance to handle form data content type

--- Workflow GPP/mini-project/cms/src/Api.js
+++ Workflow GPP/mini-project/cms/src/Api.js
@@ -30,4 +30,38 @@ api.interceptors.response.use(
   }
 );
 
-export default api;
+const apiFormData = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+});
+
+apiFormData.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    // handle 401 error (unauthorized)
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        store.dispatch(refreshToken());
+        return api(originalRequest);
+      } catch (refreshError) {
+        // redirect to login page
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+const AxiosInstance = {
+  api,
+  apiFormData,
+};
+
+// export default api;
+export default AxiosInstance;
